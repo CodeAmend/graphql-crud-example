6088e82e908b3c6e97af399d83ea195fc9b6bd06
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-08-06 22:55:02 -0500

    Customer root query with resolve

--- schema.js
+++ schema.js
@@ -2,8 +2,47 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLList,
+  GraphQLInt,
   GraphQLSchema,
   GraphQLNonNull
 } = require('graphql');
 
-module.exports = new GraphQLSchema({});
+const customers = [
+  { id: 1, name: "John Doe", age: 34, email: "john@gmail.com" },
+  { id: 2, name: "April Last", age: 39, email: "amy@gmail.com" },
+  { id: 2, name: "Ben Todd", age: 11, email: "ben@gmail.com" },
+]
+
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+});
+
+// Root Query
+const rootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+    fields: {
+      customer: { 
+        type: CustomerType,
+        args: {
+          id: { type: GraphQLString }
+        },
+        resolve(parentValue, args ) {
+          for(let i = 0; i < customers.length; i++) {
+            if(args.id == customers[i].id) {
+              return customers[i];
+            }
+          }
+        }
+      },
+    }
+}) 
+
+module.exports = new GraphQLSchema({
+  query: rootQuery
+});
