7428ab19785e85b448d8612c6a90856fdb649f5a
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-08-06 22:03:43 -0500

    Basic GraphQL setup

---
+++ schema.js
@@ -0,0 +1,9 @@
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLSchema,
+  GraphQLNonNull
+} = require('graphql');
+
+module.exports = new GraphQLSchema({});
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const expressGraphQL = require("express-graphql");
+const schema = require("./schema.js");
 
 const app = express();
+
+
+app.use("/graphql", expressGraphQL({
+  schema,
+  graphiql: true
+}));
+
 
 const port = 4000;
 
